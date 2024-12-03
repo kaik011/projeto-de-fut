59a6b76a24d04b912419c575d2c3a914dad68aa3
kaik011 <00001072730790sp@al.educacao.sp.gov.br>
2024-12-03 13:35:24 -0300

    adicionando arquivos do fut

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'esporte',
+    'quem é o rei do futebol?',
+    'é o Pelé'
+)
+
+criaCartao(
+    'esporte',
+    'quem é a rainha do futbol?',
+    'é a marta '
+)
+
+criaCartao(
+    'Esporte',
+    'Qual é a seleção com mais copa do mundo ?',
+    'é a seleção brasileira'
+)
+
+criaCartao(
+    'esporte',
+    'quem foi a seleção campeã da ultima copa do mundo',
+    'foi a seleção argentina'
+)
